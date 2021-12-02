fc806e23b6640be8e39e84ec9d1a6b7d170bc030
RonaldoYusuf <90240652+ronaldoyusuf@users.noreply.github.com>
2021-12-02 13:30:00 +0800

    Add files via upload

---
+++ Quiz2PraktikumPBO/src/quiz2praktikumpbo/quiz2no1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package quiz2praktikumpbo;
+
+/**
+ *
+ * @author HP
+ */
+public class quiz2no1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
